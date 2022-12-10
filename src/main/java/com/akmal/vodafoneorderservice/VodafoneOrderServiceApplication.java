70417ebfcfe6414d4bdaa386ab3761c963c07d8f
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-12-10 07:25:04 +0100

    Initial commit: Spring Boot project set up

---
+++ src/main/java/com/akmal/vodafoneorderservice/VodafoneOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akmal.vodafoneorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodafoneOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodafoneOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akmal/vodafoneorderservice/VodafoneOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akmal.vodafoneorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodafoneOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
